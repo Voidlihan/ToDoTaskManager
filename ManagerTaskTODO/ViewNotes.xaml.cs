c8ac88f08fab4c79fbca45dccf0abfe37924df08
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-18 21:40:09 +0600

    Add project files.

---
+++ ManagerTaskTODO/ViewNotes.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ManagerTaskTODO
+{
+    /// <summary>
+    /// Interaction logic for ViewNotes.xaml
+    /// </summary>
+    public partial class ViewNotes : Window
+    {
+        public ViewNotes()
+        {
+            InitializeComponent();
+        }
+    }
+}
